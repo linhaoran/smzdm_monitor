9609a49056147e173d473387f8da5da885d89371
Lin Haoran <linhaoran@163.com>
2016-01-31 15:07:32 +0800

    剔除Helloworld2文件中的一行注释
    实际是为了测试GITHUB的免密码提交。

--- helloworld2.py
+++ helloworld2.py
@@ -3,7 +3,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        # self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World! from Lin Haoran')
 
 
